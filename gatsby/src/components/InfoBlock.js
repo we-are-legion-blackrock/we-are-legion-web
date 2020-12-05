47e690991206cf07e2da8ab56205ff0fb5671234
Soockee <gamingsockee@gmail.com>
2020-12-05 16:46:39 +0100

    info udpate

--- gatsby/src/components/InfoBlock.js
+++ gatsby/src/components/InfoBlock.js
@@ -5,8 +5,10 @@ export const InfoBlock = withMenuComponentBlock({
   id: 'info',
   Component: () => <div className='info-column'>
      <div className='has-text-left-desktop has-text-centered-mobile is-size-5-desktop is-size-5-touch'>
-     „We are Legion“ ist eine Gilde, welche den sozialen Aspekt sowie den einer Semi-Progress Gilde vereint. 
-     Unser aktuelles Ziel ist es unter den besten 1000 Gilden der Welt zu spielen. 
+     „We are Legion“ vereint Gemeinschaftlichkeit und Progress in einer Gilde. Erfolge wie Cutting Edge und Tequilla auf Ex können wir unser Eigen nennen.
+     Unser aktuelles Ziel ist es unter den besten 1000 Gilden der Welt zu spielen.
+
+     "Eine Gemeinschaft wird nicht durch ihre Erfolge geprägt, sondern wie sie mit Misserfolgen umgehen kann" 
     </div>
   </div>,
 });
