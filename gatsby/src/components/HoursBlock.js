1420401e775688efbdd76d6ed9866c28e0dcd306
Soockee <gamingsockee@gmail.com>
2020-12-05 16:37:40 +0100

    changed times

--- gatsby/src/components/HoursBlock.js
+++ gatsby/src/components/HoursBlock.js
@@ -5,7 +5,7 @@ export const HoursBlock = withMenuComponentBlock({
   id: 'hours',
   Component: () => <div className='hours-column'>
      <div className='has-text-left-desktop has-text-centered-mobile is-size-3-desktop is-size-5-touch'>
-      <span className='brand-black-underline'>Raidzeiten</span> sind Montags und Mittwochs von 19:45 bis 23:00
+      <span className='brand-black-underline'>Raidzeiten</span> sind Montags und Donnerstag von 19:45 bis 23:00
     </div>
   </div>,
 });
